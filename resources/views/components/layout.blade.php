faa1b9e94cb145a92247de8c73377952e88e37c5
parthpathak719 <bl.en.u4cse23036@bl.students.amrita.edu>
2025-08-29 02:03:17 +0530

    dsf

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/wizards.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/recruit.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/cast.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/quiz.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/quiz.css') }}"
 
     {{ $head ?? '' }}
 </head>
